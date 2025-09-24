1f8d32239cf74b399c1f4ab6a9a8b876c27c7342
Paul <aliceapa111@gmail.com>
2025-09-24 01:45:39 -0400

    Added screen transition

--- Assets/Game/MapTransition.cs
+++ Assets/Game/MapTransition.cs
@@ -7,15 +7,17 @@ public class MapTransition : MonoBehaviour
     [SerializeField] PolygonCollider2D mapBoundary;
     CinemachineConfiner2D confiner;
 
-private void Awake()
-{
-    confiner = FindFirstObjectByType<CinemachineConfiner2D>();
-}
-
-private void OnTriggerEnter2D(Collider2D collision)
+    private void Awake()
     {
+        confiner = FindAnyObjectByType<CinemachineConfiner2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Contact");
         if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("Contact Player");
             confiner.BoundingShape2D = mapBoundary;
         }
     }
