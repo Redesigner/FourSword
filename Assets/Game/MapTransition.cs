9b0fa7d97c6af30e373e5b8b61db80db55214f02
Paul <aliceapa111@gmail.com>
2025-09-24 00:20:53 -0400

    Continued development of room transitions, camera follow, and placeholder tiles

---
+++ Assets/Game/MapTransition.cs
@@ -0,0 +1,22 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundary;
+    CinemachineConfiner2D confiner;
+
+private void Awake()
+{
+    confiner = FindFirstObjectByType<CinemachineConfiner2D>();
+}
+
+private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundary;
+        }
+    }
+}
