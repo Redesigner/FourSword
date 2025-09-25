3dadfd7bce93629f7a925560448a7d5deed03753
Paul <aliceapa111@gmail.com>
2025-09-24 21:06:51 -0400

    Finished Map transition entity

--- Assets/Game/MapTransition.cs
+++ Assets/Game/MapTransition.cs
@@ -6,19 +6,47 @@ public class MapTransition : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D mapBoundary;
     CinemachineConfiner2D confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePos = 2f;
+    
+    enum Direction { Up, Down, Left, Right }
 
     private void Awake()
     {
         confiner = FindAnyObjectByType<CinemachineConfiner2D>();
     }
 
+    //Trigger for when a player enters a transition waypoint
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Contact");
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Contact Player");
             confiner.BoundingShape2D = mapBoundary;
+            updatePlayerPosition(collision.gameObject);
         }
+    }
+
+    //Puts player a few units forward when going into a room
+    private void updatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos;
+                break;
+            case Direction.Down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x += additivePos;
+                break;
+            case Direction.Right:
+                newPos.x -= additivePos;
+                break;
+        }
+        
+        player.transform.position = newPos;
     }
 }
