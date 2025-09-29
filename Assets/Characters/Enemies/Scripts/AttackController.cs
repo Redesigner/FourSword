2dda6bce1a6f016eb92877044178ef9e9c9dbac3
steph <stephenwmelnick@gmail.com>
2025-09-29 18:07:25 -0400

    Added basic hitbox attack logic

--- Assets/Characters/Enemies/Scripts/AttackController.cs
+++ Assets/Characters/Enemies/Scripts/AttackController.cs
@@ -1,4 +1,6 @@
-﻿using Characters;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Characters;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -14,9 +16,18 @@ public class AttackController : MonoBehaviour
 
     private Animator _animator;
 
+    private List<HealthComponent> _enemiesHit = new();
+    private List<HitboxTrigger> _hitboxes = new();
+    
     protected void OnEnable()
     {
         _animator = GetComponent<Animator>();
+
+        _hitboxes = GetComponentsInChildren<HitboxTrigger>().ToList();
+        foreach (var hitbox in _hitboxes)
+        {
+            hitbox.hitboxOverlapped.AddListener(OnHitboxOverlapped);
+        }
     }
 
     public void OnAttackInput(InputAction.CallbackContext context)
@@ -33,11 +44,39 @@ public class AttackController : MonoBehaviour
     {
         // hitboxComponent.Reset();
         _animator.SetTrigger(AttackTrigger);
+        
+        foreach (var hitbox in _hitboxes)
+        {
+            hitbox.Enable();
+        }
     }
 
     public void CompleteAttack()
     {
         // Should be called from animation (if used)
         attackCompleted.Invoke();
+        _enemiesHit.Clear();
+
+        foreach (var hitbox in _hitboxes)
+        {
+            hitbox.Disable();
+        }
+    }
+
+    private void OnHitboxOverlapped(Collider2D hitbox, Collider2D hitboxOther)
+    {
+        var enemyHealth = hitboxOther.GetComponent<HealthComponent>();
+        if (!enemyHealth)
+        {
+            return;
+        }
+
+        if (_enemiesHit.Contains(enemyHealth))
+        {
+            return;
+        }
+        
+        _enemiesHit.Add(enemyHealth);
+        Debug.Log("Hit something!");
     }
 }
