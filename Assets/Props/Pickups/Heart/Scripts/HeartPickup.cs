4441c5a9480b50aca76ab4849684a74066f5fd60
steph <stephenwmelnick@gmail.com>
2025-10-22 15:43:53 -0400

    Added health pickup

---
+++ Assets/Props/Pickups/Heart/Scripts/HeartPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Props.Pickups.Heart
+{
+    public class HeartPickup : Pickup
+    {
+        [SerializeField] [Min(0.0f)] private float healing = 5.0f;
+        protected override void PlayerPickedUp(GameObject player)
+        {
+            var healthComponent = player.GetComponent<HealthComponent>();
+            if (!healthComponent)
+            {
+                return;
+            }
+            
+            healthComponent.Heal(healing);
+            Destroy(gameObject);
+        }
+    }
+}
