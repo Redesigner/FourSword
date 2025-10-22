633ef504afe0927ce1a60d7f214aee8bcf2954e5
steph <stephenwmelnick@gmail.com>
2025-10-21 21:23:29 -0400

    Added leaf particle test

--- Assets/Props/Scripts/BreakableObject.cs
+++ Assets/Props/Scripts/BreakableObject.cs
@@ -1,16 +1,33 @@
 ﻿using Shared;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Props.Scripts
 {
     public class BreakableObject : DamageListener
     {
+        [SerializeField] private GameObject particlePrefab;
+        [SerializeField] [Min(0.0f)] private float particleLifetime;
+        
         public override void TakeDamage(float damage, GameObject source, DamageType damageType = DamageType.Raw)
         {
-            if (damageType == DamageType.Slashing)
+            if (damageType != DamageType.Slashing)
             {
-                Destroy(gameObject);
+                return;
             }
+            
+            Destroy(gameObject);
+            if (!particlePrefab)
+            {
+                return;
+            }
+
+            var particleObject = Instantiate(particlePrefab);
+            particleObject.transform.position = transform.position;
+            TimerManager.instance.CreateTimer(particleObject, particleLifetime, () =>
+            {
+                Destroy(particleObject);
+            });
         }
     }
 }
